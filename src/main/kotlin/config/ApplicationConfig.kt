9be1ecc03c695fb5c70396d584e1aae9ff7095f4
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-07-08 17:30:35 +0300

    update for ConfigProviderTest(part2)

--- src/main/kotlin/config/ApplicationConfig.kt
+++ src/main/kotlin/config/ApplicationConfig.kt
@@ -3,7 +3,7 @@ package kotlintest
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ApplicationConfig ( val host: String, val user: String, val password: Int , val landingEndpoint: String?)
+data class ApplicationConfig(val host: String, val user: String, val password: Int, val landingEndpoint: String?)
 
 
 
--- src/main/kotlin/config/ConfigProvider.kt
+++ src/main/kotlin/config/ConfigProvider.kt
@@ -1,19 +1,21 @@
+package config
+
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import kotlintest.ApplicationConfig
-import javax.naming.Context
 
 interface ConfigProvider {
     fun getConfig(): ApplicationConfig
 }
 
 class MmConfigProvider : ConfigProvider {
+    private val filepath: String = "config\\config.yaml"
     override fun getConfig(): ApplicationConfig {
-        return Thread.currentThread().contextClassLoader.getResourceAsStream("config\\configTest")?.use {
+        return Thread.currentThread().contextClassLoader.getResourceAsStream(filepath)?.use {
             ObjectMapper(YAMLFactory())
-                    .registerModule(KotlinModule())
-                    .readValue(it, ApplicationConfig::class.java)
-        }!!
+                .registerModule(KotlinModule())
+                .readValue(it, ApplicationConfig::class.java)
+        } ?: throw IllegalStateException("Could not get Application Config object")
     }
 }
