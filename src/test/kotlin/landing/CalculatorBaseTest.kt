43fce91225be7caab22f06742eef3039de927d0f
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-08-17 03:15:43 +0300

    LandingPageCheckTest - input data

--- src/test/kotlin/landing/CalculatorBaseTest.kt
+++ src/test/kotlin/landing/CalculatorBaseTest.kt
@@ -1,25 +1,27 @@
 package landing
 
+import config.ConfigProviderManager
+import config.FileType
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import java.util.concurrent.TimeUnit
 
-abstract class BaseTest {
-    lateinit var driver: WebDriver
+abstract class CalculatorBaseTest {
+    val driver: WebDriver = ChromeDriver()
 
     @BeforeEach
     fun init() {
-        driver = ChromeDriver()
-        driver.get("https://moneyman:1005@qa-delivery-mx-master.moneyman.ru")
+        val landingPage = ConfigProviderManager().setFileType(FileType.YAML).getConfig()
+        driver.get("https://" + landingPage.user + ":" + landingPage.password + "@" + landingPage.host)
         System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDriver\\chromedriver.exe")
         (driver as ChromeDriver).manage().window().maximize()
-        (driver as ChromeDriver).manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
     }
 
     @AfterEach
     fun quitSession() {
-//        driver.quit()
+        driver.quit()
     }
 }
