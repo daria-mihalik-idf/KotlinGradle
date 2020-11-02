0a6cf2b9e25770f8b3637b0b2b03d70d83d4768a
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-11-02 11:40:46 +0300

    WebDriverManager - update(singleton)

--- src/main/kotlin/core/driver/WebDriverManager.kt
+++ src/main/kotlin/core/driver/WebDriverManager.kt
@@ -1,11 +1,24 @@
 package core.driver
 
+import org.openqa.selenium.WebDriver
+
 class WebDriverManager(private var webDriverConfig: WebDriverConfig) {
 
-  fun getDriverFactory(): WebDriverFactory {
-    return when (webDriverConfig.browserType) {
-      "CHROME" -> ChromeDriverFactory(webDriverConfig)
-      "FIREFOX" -> FirefoxDriverFactory(webDriverConfig)
+  companion object {
+    var driver: WebDriver? = null
+  }
+
+  fun getDriver(): WebDriver {
+    if (driver == null) {
+      initDriver()
+    }
+    return driver!!
+  }
+
+  private fun initDriver() {
+    driver = when (webDriverConfig.browserType) {
+      "CHROME" -> ChromeDriverFactory(webDriverConfig).getDriver()
+      "FIREFOX" -> FirefoxDriverFactory(webDriverConfig).getDriver()
       else -> throw IllegalArgumentException("WebDriverFactory not defined for browser ${webDriverConfig.browserType}")
     }
   }
