0a70eb2b4c8653ab3f8ddd851e93708c8701b0bd
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-12-09 16:00:58 +0300

    Selenide (factory, wrappers update)

--- src/test/kotlin/ui/UiBaseTest.kt
+++ src/test/kotlin/ui/UiBaseTest.kt
@@ -1,7 +1,6 @@
 package ui
 
 import com.codeborne.selenide.Selenide
-import com.codeborne.selenide.WebDriverRunner
 import core.config.ApplicationConfig
 import core.config.ApplicationConfigProviderManager
 import core.config.FileType
@@ -10,25 +9,23 @@ import core.driver.WebDriverConfigProviderManager
 import core.driver.selenide.SelenideDriverManager
 import core.driver.selenium.WebDriverManager
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
-import org.openqa.selenium.WebDriver
 
 abstract class UiBaseTest {
-  lateinit var driver: WebDriver
   lateinit var applicationConfig: ApplicationConfig
   private lateinit var webDriverConfig: WebDriverConfig
+
+  @BeforeAll
+  fun configureUrl() {
+    applicationConfig = ApplicationConfigProviderManager().getConfig(FileType.YAML)
+  }
 
   @BeforeEach
   fun init() {
     webDriverConfig = WebDriverConfigProviderManager().getConfig(FileType.YAML)
     SelenideDriverManager.getDriverFactory(webDriverConfig).configDriver()
-    selectBrowser()
-  }
-
-  private fun selectBrowser() {
-    applicationConfig = ApplicationConfigProviderManager().getConfig(FileType.YAML)
     Selenide.open(applicationConfig.getBaseUrlWithAuthorization())
-    driver = WebDriverRunner.getWebDriver()
   }
 
   @AfterEach
