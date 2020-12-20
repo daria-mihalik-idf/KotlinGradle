ac3e7843f0c365e7b06ee264637f87a1ce4f0d47
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-12-20 22:11:13 +0300

    Update waiter

--- src/main/kotlin/ui/waiter/CustomWaiter.kt
+++ src/main/kotlin/ui/waiter/CustomWaiter.kt
@@ -1,12 +1,9 @@
 package ui.waiter
 
 import com.codeborne.selenide.Condition
-import com.codeborne.selenide.Driver
 import com.codeborne.selenide.Selenide.`$`
-import com.codeborne.selenide.conditions.AttributeWithValue
-import com.codeborne.selenide.impl.Html
 import org.openqa.selenium.By
-import org.openqa.selenium.WebElement
+import ui.waitсondition.WaitAttributeContainsTextCondition
 
 object CustomWaiter {
   private const val DEFAULT_POLLING_TIMEOUT_SECONDS: Long = 3
@@ -17,43 +14,31 @@ object CustomWaiter {
     `$`(locator).waitUntil(Condition.text(value), timeout)
   }
 
-  fun waitForElementAttributeLoad(locator: By, attributeName: String, attributeText: String, timeout:
-  Long =
-      DEFAULT_WAIT_TIMEOUT_SECONDS) {
+  fun waitForElementAttributeLoad(
+      locator: By,
+      attributeName: String,
+      attributeText: String,
+      timeout: Long = DEFAULT_WAIT_TIMEOUT_SECONDS) {
     `$`(locator).waitUntil(Condition.attribute(attributeName, attributeText), timeout)
   }
 
-  fun waitForAttributeAndTextElementLoad(locator: By, attributeName: String, attributeText: String, value: String) {
-    `$`(locator).shouldHave(
-        Condition.attribute(attributeName, attributeText),
-        Condition.text(value))
+  fun waitForAttributeAndTextElementLoad(
+      locator: By,
+      attributeName: String,
+      attributeText: String,
+      value: String, timeout: Long = DEFAULT_WAIT_TIMEOUT_SECONDS) {
+    `$`(locator).waitUntil(
+        Condition.and(Condition.attribute(attributeName, attributeText).toString(), Condition.text(value)
+        ), timeout
+    )
   }
 
-  fun waitAttributeContainsText(locator: By, attributeName: String, attributeText: String, timeout: Long =
-      DEFAULT_WAIT_TIMEOUT_SECONDS) {
-    `$`(locator).shouldHave(
-        WaitAttributeContainsTextCondition(attributeName, attributeText))
-  }
-}
-
-class WaitAttributeContainsTextCondition(private val attributeName: String,
-    private val expectedAttrValue: String) :
-    AttributeWithValue(attributeName, expectedAttrValue) {
-
-  override fun apply(driver: Driver, element: WebElement): Boolean {
-    return Html.text.contains(getValueAttribute(element), expectedAttrValue)
-  }
-
-  override fun actualValue(driver: Driver, element: WebElement): String {
-    return String.format("%s=\"%s\"", attributeName, getValueAttribute(element))
-  }
-
-  override fun toString(): String {
-    return String.format("%s %s=\"%s\"", name, attributeName, expectedAttrValue)
-  }
-
-  private fun getValueAttribute(element: WebElement): String {
-    val attr = element.getAttribute(attributeName)
-    return attr ?: ""
+  fun waitAttributeContainsText(
+      locator: By,
+      attributeName: String,
+      attributeText: String,
+      timeout: Long = DEFAULT_WAIT_TIMEOUT_SECONDS) {
+    `$`(locator).waitUntil(
+        WaitAttributeContainsTextCondition(attributeName, attributeText), timeout)
   }
 }
